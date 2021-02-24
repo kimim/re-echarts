5fc6a8380654806c8fc82d85ac8d875652efcd9a
kimim <kimi.im@outlook.com>
2021-02-24 17:52:03 +0800

    add example

--- project.clj
+++ project.clj
@@ -10,5 +10,9 @@
   [[org.clojure/clojurescript "1.10.520" :scope "provided"]
    [reagent "1.0.0"]]
 
+  :repositories
+  {"clojars" {:url "https://clojars.org/repo"
+              :sign-releases false}}
+
   :source-paths
   ["src"])
